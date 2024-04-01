0eb6ffc9d0b087abf1869d9e4ed23a16889444ed
juanxo13597 <juanxo97master@gmail.com>
2024-04-01 11:24:44 +0200

    ci(auth): :page_facing_up: ci

--- src/features/auth/auth.controller.spec.ts
+++ src/features/auth/auth.controller.spec.ts
@@ -1,7 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { MockPrismaService } from '../prisma/prisma.service.mock';
 import { PrismaService } from '../prisma/prisma.service';
 
 describe('AuthController', () => {
@@ -11,10 +10,7 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [
-        AuthService,
-        { provide: PrismaService, useClass: MockPrismaService },
-      ],
+      providers: [AuthService, PrismaService],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
