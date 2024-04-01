168f8f7552a06b88e527fc9e8403db5e437c5661
juanxo13597 <juanxo97master@gmail.com>
2024-04-01 13:25:40 +0200

    refactor(auth): :art: generate new method for compare and hash password

--- src/features/auth/auth.service.ts
+++ src/features/auth/auth.service.ts
@@ -8,12 +8,16 @@ import { LoginDTO, RegisterDTO } from './DTOS';
 @Injectable()
 export class AuthService {
   constructor(private readonly prismaService: PrismaService) {}
+
   async register(registerDTO: RegisterDTO) {
     if (registerDTO.password !== registerDTO.password_confirmation) {
-      throw new HttpException('Invalid passwords', HttpStatus.BAD_REQUEST);
+      throw new HttpException(
+        'Las contraseñas no coinciden',
+        HttpStatus.BAD_REQUEST,
+      );
     }
 
-    const hashPassword = await bcrypt.hash(registerDTO.password, SALT_CRYPTO);
+    const hashPassword = await this.hashPassword(registerDTO.password);
     const data = { email: registerDTO.email, password: hashPassword };
 
     return this.prismaService.user
@@ -22,7 +26,6 @@ export class AuthService {
         if (error.code === 'P2002') {
           const model = String(error.meta.modelName);
           const key = String(error.meta.target);
-
           throw new HttpException(
             `Unique constraint failed on the model ${model} and key ${key}`,
             HttpStatus.CONFLICT,
@@ -37,16 +40,36 @@ export class AuthService {
       where: { email: loginDTO.email },
     });
 
-    if (!user) throw new HttpException('Invalid user', HttpStatus.UNAUTHORIZED);
+    if (!user) {
+      throw new HttpException(
+        'Contraseña o email inválidos',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
 
-    const passwordMatch = await bcrypt.compare(
+    const passwordMatch = await this.comparePasswords(
       loginDTO.password,
       user.password,
     );
+
     if (!passwordMatch) {
-      throw new HttpException('Invalid user', HttpStatus.UNAUTHORIZED);
+      throw new HttpException(
+        'Contraseña o email inválidos',
+        HttpStatus.UNAUTHORIZED,
+      );
     }
 
     return { id: user.id, email: user.email };
+  }
+
+  private async hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, SALT_CRYPTO);
+  }
+
+  private async comparePasswords(
+    plainPassword: string,
+    hashedPassword: string,
+  ): Promise<boolean> {
+    return bcrypt.compare(plainPassword, hashedPassword);
   }
 }
