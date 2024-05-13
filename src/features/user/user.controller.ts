c5a3288a40bc870965b541925a52c4c73e77a37a
juan jose navarro perea <juanxo97master@gmail.com>
2024-05-13 17:36:41 +0200

    feat: add codumentation swagger

--- src/features/user/user.controller.ts
+++ src/features/user/user.controller.ts
@@ -1,12 +1,13 @@
 import {
   Controller,
   Get,
+  HttpCode,
   HttpException,
   HttpStatus,
   Param,
   UseGuards,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { AuthGuard } from '../auth/auth.guard';
 
@@ -15,12 +16,32 @@ import { AuthGuard } from '../auth/auth.guard';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'listado de usuarios',
+  })
+  @ApiBearerAuth('APIKey-auth')
+  @HttpCode(HttpStatus.OK)
   @UseGuards(AuthGuard)
   @Get()
   findAll() {
     return this.userService.findAll();
   }
 
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: '{{id}} invalida',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: '{{id}} no encontrada',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'datos del usuario {{id}}',
+  })
+  @ApiBearerAuth('APIKey-auth')
+  @HttpCode(HttpStatus.OK)
   @UseGuards(AuthGuard)
   @Get(':id')
   findById(@Param('id') id) {
