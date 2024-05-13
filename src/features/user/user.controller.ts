4b91ef05d62068ace894380c465fa139ea1eaa1a
juan jose navarro perea <juanxo97master@gmail.com>
2024-05-13 17:33:48 +0200

    feat: find user by id

--- src/features/user/user.controller.ts
+++ src/features/user/user.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { AuthGuard } from '../auth/auth.guard';
@@ -12,5 +19,14 @@ export class UserController {
   @Get()
   findAll() {
     return this.userService.findAll();
+  }
+
+  @UseGuards(AuthGuard)
+  @Get(':id')
+  findById(@Param('id') id) {
+    const validId = Number(id);
+    if (isNaN(validId))
+      throw new HttpException('Invalid ID', HttpStatus.BAD_REQUEST);
+    return this.userService.findUserById(validId);
   }
 }
--- src/features/user/user.service.ts
+++ src/features/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
@@ -14,5 +14,12 @@ export class UserService {
         updatedAt: true,
       },
     });
+  }
+
+  async findUserById(id: number) {
+    const user = await this.prismaService.user.findUnique({ where: { id } });
+
+    if (!user) throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    return user;
   }
 }
