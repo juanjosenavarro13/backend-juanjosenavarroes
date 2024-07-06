1b77420f72d312b04ec1ddca0840d8ae3cb6d52e
juan jose navarro perea <juanxo97master@gmail.com>
2024-07-06 22:41:35 +0200

    feat: add log to health module

--- src/features/health/health.controller.ts
+++ src/features/health/health.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import {
   DiskHealthIndicator,
@@ -10,19 +10,27 @@ import {
 @ApiTags('Health')
 @Controller('health')
 export class HealthController {
+  private logger: Logger;
   constructor(
     private readonly health: HealthCheckService,
     private readonly disk: DiskHealthIndicator,
     private readonly memory: MemoryHealthIndicator,
-  ) {}
+  ) {
+    this.logger = new Logger('HEALTH');
+  }
 
   @Get()
   @HealthCheck()
-  check() {
-    return this.health.check([
+  async check() {
+    const health = await this.health.check([
       () => this.memory.checkHeap('memory_heap', 150 * 1024 * 1024),
       () =>
-        this.disk.checkStorage('storage', { path: '/', thresholdPercent: 0.5 }),
+        this.disk.checkStorage('storage', {
+          path: '/',
+          threshold: 0.9,
+        }),
     ]);
+    this.logger.log('health', health);
+    return health;
   }
 }
